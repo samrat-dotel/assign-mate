ebd64077332b86c8308783642f3d86190dcf2785
samrat-dotel <samratdotel7264@gmail.com>
2026-06-19 13:29:18 +0545

    initialize login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,7 +4,14 @@ import styles from "./Login.module.css";
 const Login = () => {
   return (
     <main className={styles.main}>
-      <h1> Hello World</h1>
+      <div className={styles.title}>logo h1 p</div>
+
+      <form action="/submit" method="post"></form>
+
+      <p>
+        {" "}
+        New here? <a href="/register"> Join the group </a>
+      </p>
     </main>
   );
 };
